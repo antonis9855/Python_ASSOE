7c854597b5183e4a105f30daccecbc3c36e68d57
Antonis <153869185+antonis9855@users.noreply.github.com>
2023-12-15 18:54:42 +0200

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,335 @@
+"""Seira Askhsewn 2."""
+
+
+def sum_sequence(n, term):
+    """A8roisma arxikwn orwn akolou8ias
+
+    n -- deikths teleutaiou orou (deikths prwtou = 1)
+    term -- synarthsh: term(i) einai o i-ostos oros
+
+    Epistrefei tin timi term(1) + term(2) + ... + term(n).
+    """
+    i, sum = 2, term(1)
+    while i <= n:
+        sum += term(i)
+        i += 1
+    return sum
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 1 <<<<<<<<<<<<<<<
+
+def digit_sum(x):
+    """Ypologizei to a8roisma twn (dekadikwn) pshfiwn tou x
+
+    x -- 8etikos akeraios
+
+    >>> digit_sum(10000)
+    1
+    >>> digit_sum(615)
+    12
+    >>> digit_sum(23) == 5
+    True
+    """
+    s = 0
+    while x>0:
+        s = s + (x%10)
+        x = x//10
+
+    return s
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 2 <<<<<<<<<<<<<<<
+
+def count_digit(x, i):
+    """Ypologizei to plh8os twn emfanisewn enos pshfiou
+
+    x -- 8etikos akeraios
+    i -- pshfio (1 = 1o pshfio apo dexia, 2 = 2o apo dexia, ktl.)
+
+    >>> count_digit(1000, 1)
+    3
+    >>> count_digit(12944342, 2)
+    3
+    >>> count_digit(121,1) == 2
+    True
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+
+    s=0
+    digit=(x//(10**(i-1)))%10
+    while x>0:
+        if x%10==digit:
+            s=s+1
+        x=x//10
+    return s
+        
+
+  
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 3 <<<<<<<<<<<<<<<
+
+def paei_pi(n):
+    """A8roisma n orwn tis akoloy8ias 
+              4, -4/3, 4/5, -4/7, 4/9, -4/11, 4/13, ... ktl.
+
+    n -- 8etikos akeraios >= 1
+
+    Epistrefei to a8roisma twn orwn 1 ews kai n.
+
+    Paradeigmata:
+    >>> paei_pi(1)
+    4.0
+    >>> paei_pi(2)
+    2.666666666666667
+    >>> paei_pi(3)
+    3.466666666666667
+    >>> paei_pi(1000)
+    3.140592653839794
+    >>> paei_pi(10000)
+    3.1414926535900345
+    """
+    def sum_div(x):
+        if x%2!=0:
+            return 4/(1+((x-1)*2))
+        elif x%2==0:
+            return -(4/(1+((x-1)*2)))
+
+    return sum_sequence(n, sum_div)
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 4 <<<<<<<<<<<<<<<
+
+def stars_horizontal(n):
+    """String asteriwn topo8etimena orizontiws.
+
+    n -- akeraios >= 1
+    
+    Epistrefei string me n xaraktires '*'.
+
+    Paradeigmata:
+    >>> stars_horizontal(1)
+    '*'
+    >>> stars_horizontal(5)
+    '*****'
+    >>> print(stars_horizontal(5))
+    *****
+    """
+    def stars(x):
+        return '*' # mporeite na xrhsimopoihsete perissoteres 'h ligoteres grammes
+    return sum_sequence(n , stars)
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 5 <<<<<<<<<<<<<<<
+
+def reverse_args(f):
+    """ Antistrefei ta orismata.
+
+    -- f synartisi 2 orismatwn
+
+    Epistrefei synartisi g opou i klisi g(x, y) exei idia timi me tin f(y, x).
+
+    Paradeigmata:
+    >>> pow(2, 3)
+    8
+    >>> pow(3, 2)
+    9
+    >>> wop = reverse_args(pow)
+    >>> wop(2, 3)
+    9
+    >>> wop(3, 2)
+    8
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+    def g(y,x):
+        return f(x,y)
+    return g
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 6 <<<<<<<<<<<<<<<
+
+def askisi6():
+    """Ypologismoi gewmetrikwn sximatwn.
+
+    >>> askisi6()
+    Perifereies kyklwn me aktina 1 ews 10
+    6.283185307179586
+    12.566370614359172
+    18.84955592153876
+    25.132741228718345
+    31.41592653589793
+    37.69911184307752
+    43.982297150257104
+    50.26548245743669
+    56.548667764616276
+    62.83185307179586
+    Emvada tetragwnwn me pleura 1 ews 8
+    1
+    4
+    9
+    16
+    25
+    36
+    49
+    64
+    Emvado kyklou aktinas 1 = 3.141592653589793
+    Emvado kyklou aktinas 2 = 12.566370614359172
+    Emvado kyklou aktinas 3 = 28.274333882308138
+    Emvado kyklou aktinas 4 = 50.26548245743669
+    Emvado kyklou aktinas 5 = 78.53981633974483
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW, APOFEYGONTAS
+    TIN EPANALIPSI PAROMOIWN ENTOLWN """
+    from math import pi
+    def print_geometry(n,level):
+        i=1
+        while i<=n:
+            print (level(i))
+            i=i+1
+    def perifereia_kyklou(r):
+            return 2 * pi * r
+    def emvado_tetragwnou(p):
+            return p**2
+    def emvado_kyklou(r):
+            return 'Emvado kyklou aktinas ' + str(pi * (r ** 2))
+    print ('Perifereies kyklwn me aktina 1 ews 10')
+    print_geometry (10, perifereia_kyklou)
+    print ('Emvada tetragwnwn me pleura 1 ews 8')
+    print_geometry(8, emvado_tetragwnou)
+    print_geometry(5, emvado_kyklou)
+
+
+    
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 7 <<<<<<<<<<<<<<<
+
+def make_quadratic(a, b, c):
+    """Polywnymo 2ou ba8mou.
+
+    a, b, c -- ari8moi
+
+    Epistrefei thn tetragwnikh synarthsh f(x) = a*x*x+b*x+c.
+
+    Paradeigmata:
+    >>> f = make_quadratic(1, 2, 1.0)
+    >>> f(0)
+    1.0
+    >>> f(-1)
+    0.0
+    >>> f(1)
+    4.0
+    >>> f(1.5)
+    6.25
+    """
+    def func(x):
+        return a*(x**2)+b*x+c
+    return func
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 8 <<<<<<<<<<<<<<<
+
+def abacize(op):
+    """Ekdoxh abaka (ari8mhthri) enos telesti.
+
+    op -- synartisi dyo akeraiwn orismatwn
+          pou epistrefei akeraia timi
+
+    Epistrefetai nea synartisi pou dexetai ta
+    ari8mitika orismata tis op kai epistrefei ta apotelesma tis
+    xrhsimopoiwntas anaparastasi me xandres ari8mitiriou.
+
+    Paradeigmata:
+    >>> from operator import *
+    >>> baby_add = abacize(add)
+    >>> baby_add('o', 'o')
+    'oo'
+    >>> baby_add('oo', 'ooo')
+    'ooooo'
+    >>> baby_mul = abacize(mul)
+    >>> baby_mul('oo', 'ooo')
+    'oooooo'
+    >>> abacize(max)('oo', 'ooo')
+    'ooo'
+    >>> abacize(pow)('oo', 'ooo')
+    'oooooooo'
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+    def h(x, y):
+        return op(len(x), len(y))
+    return h 
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 9 <<<<<<<<<<<<<<<
+
+def sum_squares(n):
+    """A8roisma tetragwnwn.
+
+    n -- 8etikos akeraios >= 1
+
+    Epistrefei tin timi 1 + 2*2 + 3*3 + ... + n*n.
+
+    Paradeigmata:
+    >>> sum_squares(1)
+    1
+    >>> sum_squares(2)
+    5
+    >>> sum_squares(100)
+    338350
+    """
+    """PREPEI NA LEITOYRGEI ANADROMIKA, XWRIS ENTOLES
+    EPANALHPSHS OPWS while, for.
+    """
+
+    if n == 1:
+        return 1
+    else:
+        return n * n + sum_squares(n - 1)
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 10 <<<<<<<<<<<<<<<
+
+def print_digits(x):
+    """Emfanizei ta pshfia ari8mou.
+
+    x -- 8etikos akeraios >= 0
+
+    Emfanizei ena ena ta pshfia tou x arxizontas 
+    apo to pio simantiko pshfio.
+
+    Paradeigmata:
+    >>> print_digits(0)
+    0
+    >>> print_digits(2019)
+    2
+    0
+    1
+    9
+    >>> print_digits(923884)
+    9
+    2
+    3
+    8
+    8
+    4
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+    """PREPEI NA LEITOYRGEI ANADROMIKA, XWRIS ENTOLES
+    EPANALHPSHS OPWS while, for.
+    """
+    
+    if x < 10:
+        print(x)
+    else:
+        print_digits(x // 10)
+        print(x % 10)
+        
+        
